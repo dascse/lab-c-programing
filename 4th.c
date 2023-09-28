76328ec003cd75f8e55a94cd0a16d14b1f42ee22
dascse <145799966+dascse@users.noreply.github.com>
2023-09-28 21:21:15 +0530

    complete

--- 4th.c
+++ 4th.c
@@ -1,3 +1,11 @@
 //
 // Created by Admin on 9/28/2023.
 //
+#include<stdio.h>
+int main() {
+    float a, f;
+    printf("Enter the temperaturein celcius:\n");
+    scanf("%f", &a);
+    f = (a * 9 / 5) + 32;
+    printf("Enter the tempurature in fahrenheit;%f", f);
+}
---
+++ 5th.c
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 9/28/2023.
+//
+#include<stdio.h>
+int main() {
+    float a;
+    printf("Enter the amount in rupees:\n");
+    scanf("%f", &a);
+    printf("Enter the amount in dollers:%f\n", a * 0.012);
+    printf("Enter the amount in europs:%f\n", a * 0.011);
+    printf("Enter the amount in yen:%f\n", a * 1.77);
+    printf("Enter the amount in rubel:%f\n", a * 1.16);
+}
