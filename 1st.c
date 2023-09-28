d87c8bd538bb3e3d9852e22d43e150a0f5ef09ed
dascse <145799966+dascse@users.noreply.github.com>
2023-09-28 20:05:40 +0530

    complete

---
+++ 1st.c
@@ -0,0 +1,15 @@
+//
+// Created by Admin on 9/28/2023.
+//
+#include<stdio.h>
+int main() {
+    int a, b;
+    printf("Enter the two number\n");
+    scanf("%d%d", &a, &b);
+    printf("addition is%d\n", a + b);
+    printf("Substraction is%d\n", a-b);
+    printf("sum is%d\n", a*b);
+    printf("division is%d\n", a/b);
+
+    return 0;
+}
