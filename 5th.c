57efba8d2000d97694398d2c4bc0f40d700f7c09
dascse <145799966+dascse@users.noreply.github.com>
2023-09-28 21:57:57 +0530

    complete

--- 5th.c
+++ 5th.c
@@ -3,11 +3,9 @@
 //
 #include<stdio.h>
 int main() {
-    float a;
-    printf("Enter the amount in rupees:\n");
+    float a, f;
+    printf("Enter the area:\n");
     scanf("%f", &a);
-    printf("Enter the amount in dollers:%f\n", a * 0.012);
-    printf("Enter the amount in europs:%f\n", a * 0.011);
-    printf("Enter the amount in yen:%f\n", a * 1.77);
-    printf("Enter the amount in rubel:%f\n", a * 1.16);
+    f = ((3 * 1.732) / 2) * a * a;
+    printf("area of the hexagon=%f", f);
 }
