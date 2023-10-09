9f4335c162dbca422cbf104bd8f4dffe478142c1
dascse <145799966+dascse@users.noreply.github.com>
2023-10-09 19:33:18 +0530

    c

--- 38th.c
+++ 38th.c
@@ -1,3 +1,17 @@
 //
 // Created by Admin on 10/9/2023.
 //
+#include<stdio.h>
+int main() {
+    int i,b;
+    i=0;
+    int c=0;
+    printf("Enter the number");
+    scanf("%d",&b);
+    while(i<=b) {
+        c = c + i;
+        i++;
+    }
+        printf("\nsome of some is:%d",c);
+
+}
