369f711663462a05693f03cf60af6c3c87845324
dascse <145799966+dascse@users.noreply.github.com>
2023-10-09 18:55:45 +0530

    c

--- 34th.c
+++ 34th.c
@@ -1,3 +1,14 @@
-//
-// Created by Admin on 10/8/2023.
-//
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("Enter the number");
+    scanf("%d",&a);
+    printf("Enter the number");
+    scanf("%d",&b);
+    if (a ==b) {
+        printf("area of a square%d ",a*a);
+    } else {
+        printf("area of a rectangel%d",a*b);
+    }
+    return 0;
+}
