cee61326cc105d591a78ff1e2cf5af9ec53a1f8d
dascse <145799966+dascse@users.noreply.github.com>
2023-10-09 19:11:20 +0530

    c

--- 36th.c
+++ 36th.c
@@ -1,3 +1,28 @@
 //
 // Created by Admin on 10/9/2023.
 //
+#include<stdio.h>
+int main() {
+    char c;
+    printf("Enter the number");
+    scanf("%c",&c);
+     switch(c)
+     {
+         case'a':
+         case'e':
+         case'i':
+         case'o':
+         case'u':
+         case'A':
+         case'E':
+         case'I':
+         case'O':
+         case'U':
+
+         printf("Enter the number is vowel:%c",c);
+         break;
+         default:
+             printf("Enter the number is consonent:%c",c);
+     }
+    return 0;
+}
