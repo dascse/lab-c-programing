f52b3fe446a0cf09a87cda20acfc88b4f7af8ce1
dascse <145799966+dascse@users.noreply.github.com>
2023-10-09 18:56:48 +0530

    c

--- 35th.c
+++ 35th.c
@@ -1,3 +1,16 @@
 //
 // Created by Admin on 10/9/2023.
 //
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("Enter the temperature");
+    scanf("%d",&a);
+
+    if (a >=100) {
+        printf("boiling temperature");
+    } else {
+        printf("not boiling");
+    }
+    return 0;
+}
