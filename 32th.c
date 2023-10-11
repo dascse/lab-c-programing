68fa2a3131ef8baa21b00f5e0aee2ac3a25d981c
dascse <145799966+dascse@users.noreply.github.com>
2023-10-12 01:59:35 +0530

    c

--- 32th.c
+++ 32th.c
@@ -1,3 +1,31 @@
 //
 // Created by Admin on 10/8/2023.
 //
+//Write a program in c to check whether character is vowel or not in switch case.
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("Enter character:");
+    scanf("%c",&a);
+    switch(a)
+    {
+        case 'a':
+            printf("It is A");
+            break;
+        case 'e':
+            printf("It is E");
+            break;
+        case 'i':
+            printf("It is I");
+            break;
+        case 'o':
+            printf("It is O");
+            break;
+        case 'u':
+            printf("It is U");
+            break;
+        default:
+            printf("It is a Consonant");
+    }
+}
