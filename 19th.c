178b2d243896f8f3fa1f101c9b97a062fd2ff1c2
dascse <145799966+dascse@users.noreply.github.com>
2023-10-14 00:50:17 +0530

    Update 19th.c

--- 19th.c
+++ 19th.c
@@ -6,6 +6,7 @@
 int main() {
     int x, y, z;
     float a, b, c;
+    float d;
     printf("Enter the principle:");
     scanf("%d",&x);
     printf("Enter the rate");
@@ -13,9 +14,10 @@ int main() {
     printf("Enter the time");
     scanf("%d",&y);
     c = x * a * y / 100;
-    b = a * pow((c + a / 100), y);
+    d=(1+a/100);
+    b = x* pow(d,y);
     printf("Simple interest is%f\n",c);
-    printf("Compound interest is:%d\n", b);
+    printf("Compound interest is:%f\n", b);
     return 0;
 }
 
