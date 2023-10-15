dc1b99297a14311dcfef100794d21a1957b9a0e7
dascse <145799966+dascse@users.noreply.github.com>
2023-10-15 21:38:58 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,19 @@
+//
+// Created by Admin on 10/15/2023.
+//
+//Write a program in c to count the number of digits in a number.
+#include <stdio.h>
+int main()
+{
+    int n;
+    int count = 0;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    do
+    {
+        n /= 10;
+        ++count;
+    }
+    while (n != 0);
+    printf("Number of digits: %d", count);
+}
