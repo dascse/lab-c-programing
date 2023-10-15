67618f58d502d62fcbb9805a753ced25300d080c
dascse <145799966+dascse@users.noreply.github.com>
2023-10-15 21:37:50 +0530

    c

--- 49th.c
+++ 49th.c
@@ -1,1 +1,28 @@
- 
+//Write a program in c to print the following
+#include<stdio.h>
+int main()
+{
+    int row=5;
+    for(int i=0;i<2*row-1;i++)
+    {
+        int a;
+        if(i<row)
+        {
+            a=2*i+1;
+            printf(" ");
+        }
+        else
+        {
+            a=2*(2*row-i)-3;
+        }
+        for(int j=0;j<a;j++)
+        {
+            printf(" ");
+        }
+        for(int k=0;k<2*row-a;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
